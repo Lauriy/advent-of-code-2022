a35868ee17cc51c0815b3511e39274198c726857
Lauri Elias <laurileet@gmail.com>
2022-12-03 02:37:07 +0200

    Make day 1 more beautiful

--- src/day_1.py
+++ src/day_1.py
@@ -1,35 +1,34 @@
 import os
 
 
+def _extract_elves(data_lines: [str]) -> [int]:
+    current_elf_total = 0
+    elf_numbers = []
+    end = len(data_lines) - 1
+    for i, line in enumerate(data_lines):
+        if line == '':
+            elf_numbers.append(current_elf_total)
+            current_elf_total = 0
+            continue
+
+        current_elf_total += int(line)
+        if i == end:
+            elf_numbers.append(current_elf_total)
+
+    return elf_numbers
+
+
 def solve_first(file_name: str) -> int:
     with open(f"{os.path.dirname(os.path.realpath(__file__))}/../in/{file_name}", "r") as f:
         data = [x.strip() for x in f]
-    current_elf_total = 0
-    elf_numbers = []
-    for line in data:
-        try:
-            number = int(line)
-            current_elf_total += number
-        except ValueError:
-            elf_numbers.append(current_elf_total)
-            current_elf_total = 0
 
-    return max(elf_numbers)
+    return max(_extract_elves(data))
 
 
 def solve_second(file_name: str) -> int:
     with open(f"{os.path.dirname(os.path.realpath(__file__))}/../in/{file_name}", "r") as f:
         data = [x.strip() for x in f]
-    current_elf_total = 0
-    elf_numbers = []
-    for line in data:
-        try:
-            number = int(line)
-            current_elf_total += number
-        except ValueError:
-            elf_numbers.append(current_elf_total)
-            current_elf_total = 0
-
+    elf_numbers = _extract_elves(data)
     elf_numbers.sort(reverse=True)
 
     return sum(elf_numbers[:3])
